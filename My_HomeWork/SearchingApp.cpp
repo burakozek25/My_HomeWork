4b9bdcbdaacfc533cca3b162d70225e9af73d2b3
burakozek25 <ozekbu9917_aehit@students.vizja.pl>
2024-11-08 11:00:48 +0100

    Update SearchingApp.cpp

--- My_HomeWork/SearchingApp.cpp
+++ My_HomeWork/SearchingApp.cpp
@@ -2,7 +2,7 @@
 #include "Searching.cpp"
 
 int main() {
-    std::vector<int> vec = { 10, 20, 30, 30,30,40, 50, 60,70,70 }; // Already sorted
+    std::vector<int> vec = { 10, 20, 30, 30,30,40, 50, 60,70,70 }; 
     int target;
     std::cout << "Enter the value of target:";
     std::cin >> target;
